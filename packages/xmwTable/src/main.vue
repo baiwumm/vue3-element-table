23e6094935de7e4af58739b64c212760fca333ef
Cyan-Xmw <843348394@qq.com>
2022-06-14 16:53:58 +0800

    增加package.json配置项

--- packages/xmwTable/src/main.vue
+++ packages/xmwTable/src/main.vue
@@ -100,7 +100,7 @@
 </template>
 <script lang="ts">
 export default {
-  name: "vue3-xmw-table", //这个⾮常重要，就是未来你放到其他项⽬中，组件标签的名字，⽐如：<my-first-comp></my-first-comp>
+  name: "vue3-xmw-table", //这个⾮常重要，就是未来你放到其他项⽬中，组件标签的名字，⽐如：<vue3-xmw-table></vue3-xmw-table>
 };
 </script>
 <script lang="ts" setup>
